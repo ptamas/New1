363511336e54d73e5cf93ef372ecf0b975d2187a
ptamas <ptamas96@gmail.com>
2014-07-24 11:22:34 +0200

    buttonworking

---
+++ buttonworking/buttonworking.ino
@@ -0,0 +1,20 @@
+/* Blink led with button */
+
+const uint8_t LED_PIN = 13;
+const uint8_t BUTTON_PIN = 12;
+uint8_t button_state = 0;
+uint8_t led_state = LOW;
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT);
+}
+
+void loop() {
+    button_state = digitalRead(BUTTON_PIN);
+    if (button_state == LOW) {      // Button is in LOW state if it is pushed
+        led_state=!led_state;
+           digitalWrite(LED_PIN, led_state);
+    } 
+    delay(100); 
+}
